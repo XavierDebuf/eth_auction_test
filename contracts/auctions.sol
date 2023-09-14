ddae9c129baf7d7029c4a99e24dd3cff0d38da8f
XavierDebuf <x.debuf@gmail.com>
2023-09-14 09:34:42 +0200

    ajout verification bidder deja inscrit ajout modifier arret de l'enchere après 45 sec d'inactivité

--- contracts/auctions.sol
+++ contracts/auctions.sol
@@ -1,12 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Auction {
-/**owner: L'adresse du propriétaire du smart contract.
-start_time: La date et l'heure de début de l'enchère.
-reserve_price: Le prix minimum auquel l'enchère doit être vendue.
-bids:  tableau qui mappe les adresses des participants aux enchères au montant de leurs enchères. 
-*/
+contract Auction  {
     address payable owner;
     uint256 start_time;
     uint256 reserve_price;
@@ -15,14 +10,18 @@ bids:  tableau qui mappe les adresses des participants aux enchères au montant 
     //mapping(address => uint256) bids;
     address payable winner;
     struct Bidder {
-        uint256  Value;
-        address  payable addr_emitter;
+        uint256 Value;
+        address payable addr_emitter;
+        bool isRegister;
     }
     mapping (address => Bidder) bidders;
     Bidder[]  bids;
 
-    constructor(uint256 _reserve_price) public {
+    constructor(uint256 _reserve_price)  {
         reserve_price = _reserve_price;
+         Bidder memory newBidder = Bidder(0, msg.sender,true);
+        bidders[msg.sender] = newBidder;
+        bids.push(newBidder);
     }
     modifier ownable() {
         require(msg.sender != owner, "le payeur est le proprietaire");
@@ -32,6 +31,10 @@ bids:  tableau qui mappe les adresses des participants aux enchères au montant 
         require(block.timestamp<TimeDuringRegister(), "Les inscriptions sont terminees");
         _;
     }
+     modifier isBidder(){
+     require(bidders[msg.sender].isRegister==true,"le bidder doit etre inscrit");
+     _;
+    }
     modifier AuctionTime() {
         require(block.timestamp<TimeDuringAuction(), "Les inscriptions sont terminees");
         _;
@@ -39,6 +42,12 @@ bids:  tableau qui mappe les adresses des participants aux enchères au montant 
     modifier ActionAfterAuction(){
       require(block.timestamp>TimeDuringAuction(), "l'enchere n'est pas terminee");
       _;
+    }
+    modifier ActionBeforeLastBid(){
+        require(block.timestamp<TimeBeforeSold(), "l'enchere est adjugee vendu");
+    }
+    modifier ActionAfterLastBid(){
+        require(block.timestamp<TimeBeforeSold(), "l'enchere est adjugee vendu");
     }
     function TimeDuringRegister() public returns(uint256){
         start_register = block.timestamp;
@@ -48,17 +57,23 @@ bids:  tableau qui mappe les adresses des participants aux enchères au montant 
         start_time = block.timestamp;
         return (start_time +5 minutes);
     }
-    function addBidder (uint256 _value, address payable addr) public ownable register{
-        Bidder memory newBidder = Bidder(_value, addr);
+    function TimeBeforeSold() public returns(uint256) {
+        start_time = block.timestamp;
+        return(start_time + 45 seconds);
+    }
+   
+    function addBidder (uint256 _value, address payable addr, bool able2register) public ownable register{
+        require(bidders[msg.sender].isRegister,"le bidder ne doit pas etre inscrit");
+        Bidder memory newBidder = Bidder(_value, addr,true);
         bidders[msg.sender] = newBidder;
         bids.push(newBidder);
     }
-    function bid(uint256 amount) public payable ownable AuctionTime{
+    function bid(uint256 amount) public payable ownable AuctionTime isBidder ActionBeforeLastBid {
         require(amount > current_price, "le montant est trop faible");
          bidders[msg.sender].Value=amount;
          current_price = amount;
     }
-    function biggestBidder() public returns(address payable){
+    function biggestBidder() public isBidder returns(address payable) {
       uint max = 0;
       uint win = 0;
       for(uint8 i = 0; i <= bids.length; i++){
@@ -70,16 +85,16 @@ bids:  tableau qui mappe les adresses des participants aux enchères au montant 
         return (bids[win].addr_emitter);
 
     }
-    function refund() public ownable ActionAfterAuction{
+    function refund() public ownable ActionAfterAuction ActionAfterLastBid isBidder{
       for(uint8 i = 0; i <= bids.length; i++){
             (bids[i].addr_emitter).transfer(bids[i].Value);
         }
     }
-    function transfertOwnership(address payable newAddress) public ownable ActionAfterAuction{
+    function transfertOwnership(address payable newAddress) public ownable ActionAfterAuction ActionAfterLastBid isBidder{
         require(newAddress != address(0), "Adresse invalide");
         owner = newAddress;
     }
-    function auctionEnd() public ActionAfterAuction{
+    function auctionEnd() public ActionAfterLastBid ActionAfterAuction isBidder{
 
         if (current_price >= reserve_price) {
             owner.transfer(bidders[biggestBidder()].Value);
